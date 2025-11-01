db72f904b41babf200215009b1f4c4865955b540
Wangzy <wangziyuan455@Gmail.com>
2025-11-01 13:35:05 +0800

    feat: add a2a example

---
+++ spring-ai-alibaba-agent-example/a2a-client-example/src/main/java/com/cloud/alibaba/ai/example/a2a/a2aclientexample/A2aClientExampleApplication.java
@@ -0,0 +1,40 @@
+package com.cloud.alibaba.ai.example.a2a.a2aclientexample;
+
+import com.alibaba.cloud.ai.a2a.registry.nacos.discovery.NacosAgentCardProvider;
+import com.alibaba.cloud.ai.a2a.registry.nacos.utils.AgentCardConverterUtil;
+import com.alibaba.cloud.ai.graph.agent.BaseAgent;
+import com.alibaba.cloud.ai.graph.agent.a2a.A2aRemoteAgent;
+import com.alibaba.cloud.ai.graph.agent.a2a.AgentCardProvider;
+import com.alibaba.cloud.ai.graph.agent.a2a.AgentCardWrapper;
+import com.alibaba.cloud.ai.graph.exception.GraphRunnerException;
+import com.alibaba.cloud.ai.graph.exception.GraphStateException;
+import com.alibaba.cloud.ai.graph.streaming.StreamingOutput;
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@SpringBootApplication
+public class A2aClientExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2aClientExampleApplication.class, args);
+    }
+}
+
+
