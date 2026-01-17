58148c0d99898784297fefb603810d35a4bf8926
wangx <wangx.coding@gmail.com>
2026-01-17 17:01:02 +0800

    feat(minimax-chat): 新增minimax-chat模块及其Maven配置文件

---
+++ spring-ai-alibaba-chat-example/minimax-chat/src/main/java/com/alibaba/cloud/ai/MiniMaxApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.cloud.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangx
+ * @description
+ * @create 2026/1/17 16:44
+ */
+@SpringBootApplication
+public class MiniMaxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MiniMaxApplication.class, args);
+    }
+}
